7edbf0ddb859db5364e92fc854ad981d5f54943e
Ann <an1995ny@mail.ru>
2025-08-13 21:52:01 +0300

    first try

---
+++ src/components/counter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+function Counter() {
+    const [count, setCount] = useState(0);
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+    const handleDecrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div>
+            <h2>Счетчик: {count} </h2>
+            <button onClick={handleIncrement}>Увеличить</button>
+            <button onClick={handleDecrement}>Уменьшить</button>
+        </div>
+    )
+}
+
+export default Counter;
---
+++ src/components/timer.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+function Timer() {
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <div>
+            <h2>Текущее время:</h2>
+            <p>{time.toLocaleTimeString()}</p>
+        </div>
+    )
+}
+
+export default Timer;
